15727c1237b40fc7333849fc25a9978316ab2f0f
ThulzLoL <thulz.lol@gmail.com>
2022-02-18 00:42:31 -0300

    feat(develop): typeScript functions
    new WhackTheMole, createMole and startGame functions

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { selectWackamole, selectGamestate, selectHighscore } from './state/home.selectors';
-
+import { select, Store } from '@ngrx/store';
+import { selectWhackamole, selectGamestate, selectHighscore, selectCurrentScore } from './state/home.selectors';
+import { homeActionsGenerateMole, homeActionsToggleGame, homeActionsWhackamole } from './state/home.actions';
+import {HomeService} from './service/home.service'
+import { take } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  wackamole$ = this.store.select(selectWackamole)
+  whackamole$ = this.store.select(selectWhackamole)
   gameStarted$ = this.store.select(selectGamestate)
   highscore$ = this.store.select(selectHighscore)
+  currentScore$ = this.store.select(selectCurrentScore)
   constructor(
-    private store: Store
+    private store: Store,
+    private homeService: HomeService
   ) { }
 
   ngOnInit(): void {
   }
+  startGame(){
+    this.store.dispatch(homeActionsToggleGame({ gameState: true}));
+    this.store.dispatch(homeActionsGenerateMole())
+    this.createMole()
+    setTimeout(() => {
+      this.store.dispatch(homeActionsToggleGame({ gameState: false}));
+    }, 30000)
+  }
+  createMole(){
+    let time = this.homeService.getArbitraryNumber(1000, 3000)
+    this.store.dispatch(homeActionsGenerateMole())
+    setTimeout(async () => {
+      let gameState = await this.store.pipe(select(selectGamestate),take(1)).toPromise();
+      if(gameState){
+        this.createMole()
+      }
+    }, time)
+  }
 
+  whackedTheMole(id: string){
+    this.store.dispatch(homeActionsWhackamole({ id }));
+  }
 }
